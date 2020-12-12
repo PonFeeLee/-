0d76ee1f31a8e5e25b38fe068add6208c8e78018
PonFee <1129879311@qq.com>
2020-12-12 18:29:21 +0800

    轮播图部分 先自造固定数据

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,53 @@
 <template>
-  <div>
-    首页
+  <div class="container">
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="
+            `
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `
+          "
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      banners: [
+        {
+          url:
+            "http://img0.imgtn.bdimg.com/it/u=255540472,3722890280&fm=26&gp=0.jpg"
+        },
+        {
+          url:
+            "http://attach.bbs.miui.com/forum/201407/12/191515sskuppax42aarano.jpg"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>

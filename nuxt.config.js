c04557c445ce5fac41b0969da081fb4ad96bb80b
PonFee <1129879311@qq.com>
2020-12-12 16:45:40 +0800

    安装less axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: pkg.description }
+            { hid: "description", name: "description", content: "pkg.description" }
         ],
         link: [
             { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -46,7 +46,7 @@ export default {
      */
     modules: [
         // https://axios.nuxtjs.org/setup
-        "@nuxtjs/axios"
+        // "@nuxtjs/axios"
     ],
 
     /*

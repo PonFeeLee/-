b60a43d4f4372fdb0a020a6a4ebfeeba89fef31a
PonFee <1129879311@qq.com>
2020-12-12 16:37:02 +0800

    修改页面过渡效果样式 并 修改配置文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,72 @@
+import pkg from "./package";
+
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'xianyun',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    mode: "universal",
+
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: "练习练习练习", // 修改title
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: pkg.description }
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            {
+                rel: "stylesheet",
+                type: "text/css",
+                href: "//at.alicdn.com/t/font_1168872_ehvuah8v57g.css"
+            } // 新增全局字体样式
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#fff" },
+
+    /*
+     ** Global CSS
+     */
+    css: [
+        "element-ui/lib/theme-chalk/index.css",
+        "assets/main.css" // 新增自定义的页面过渡样式（文件来自3.4.1）
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: ["@/plugins/element-ui"],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // https://axios.nuxtjs.org/setup
+        "@nuxtjs/axios"
+    ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+        // baseURL: "http://157.122.54.189:9095" // 新增备用地址
+        baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径
+    },
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+    /*
+     ** Build configuration
+     */
+    build: {
+        transpile: [/^element-ui/],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    transpile: [/^element-ui/],
-  }
-}
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
+};
